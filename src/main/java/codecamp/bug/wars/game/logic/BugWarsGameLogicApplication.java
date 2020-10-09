0631d8cbf09fbb67b36eaa65bd175944c296a397
Jerry Balderas <Gerardo.Balderas@cognizant.com>
2020-10-09 01:22:30 -0500

    Initial commit

---
+++ src/main/java/codecamp/bug/wars/game/logic/BugWarsGameLogicApplication.java
@@ -0,0 +1,13 @@
+package codecamp.bug.wars.game.logic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BugWarsGameLogicApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BugWarsGameLogicApplication.class, args);
+	}
+
+}
---
+++ src/test/java/codecamp/bug/wars/game/logic/BugWarsGameLogicApplicationTests.java
@@ -0,0 +1,13 @@
+package codecamp.bug.wars.game.logic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BugWarsGameLogicApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
