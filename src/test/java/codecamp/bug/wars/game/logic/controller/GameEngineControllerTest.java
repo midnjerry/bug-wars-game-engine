c9c123b5a0fa6f9f8b008a34982b845980e5247f
Ramon Hubbell <Ramon.hubbell@gmail.com>
2020-11-20 16:58:53 -0600

    working on test 2

--- src/test/java/codecamp/bug/wars/game/logic/controller/GameEngineControllerTest.java
+++ src/test/java/codecamp/bug/wars/game/logic/controller/GameEngineControllerTest.java
@@ -47,26 +47,46 @@ class GameEngineControllerTest {
     public void createGame_shouldReturnGameInputAndOkHttpStatus() {
         // arrange
 //        Game postGameInput = new Game();
-
-        MapSpace[][] rows = new MapSpace[5][5];
+//      game object
+//        MapSpace[][] rows = new MapSpace[5][5];
+        MapSpace[][] rows = new MapSpace {
+            { "OPEN", "OPEN", "OPEN", "OPEN", "OPEN"},
+            { "OPEN", "WALL", "OPEN", "WALL", "OPEN"},
+            { "OPEN", "OPEN", "OPEN", "OPEN", "OPEN"},
+            {"OPEN", "WALL", "OPEN", "WALL", "OPEN"},
+            {"OPEN", "OPEN", "OPEN", "OPEN", "OPEN"}
+        }
         List<Spawn> spawns = new ArrayList<>();
         spawns.add(new Spawn(1, 0, 1));
         List<Food> food = new ArrayList<>();
         food.add(new Food(1, 1));
         Map map = new Map(rows, spawns, food);
+//        List<Food> mapFood = map.getFoods();
+//        MapSpace [][] mapRows = map.getRows();
+//        List<Spawn> mapSpawns = map.getSpawns();
 
+//        bug object
         List<Bug> bugs = new ArrayList<>();
         Integer[] code = {1, 1, 1, 1, 1, 1, 1, 1};
         bugs.add(new Bug(1, code));
 
-        Game postGameInput = new Game(map, bugs, 1, 1L);
 
+        BugResponse bugResponse = new BugResponse(2, Direction.NORTH, 3, 4, "attack", false);
+        GameState gameStateTest = new GameState(1, bugResponse);
+        Game postGameInput = new Game(map, bugs, 1, 1L);
+        Integer[] winners = {1, 2};
+        GameState [] gameStateArray = {gameStateTest};
+//        Game savedGame = new Game(map, bugs, 1, 1L);
+        Game savedGame = new Game(map,bugs  , 1, 1L);
+        GameResult expected = new GameResult(winners , "winner", gameStateArray);
+//        Mockito.when(mockAIService.saveAI(Mockito.any())).thenReturn(savedScript);
+        Mockito.when(mockGameEngineService.saveGame(Mockito.any())).thenReturn(savedGame);
 //        Act
-        ResponseEntity<GameResult> expected = new ResponseEntity(new GameResult(), HttpStatus.OK);
+        ResponseEntity<GameResult> expectedResponse = new ResponseEntity(new GameResult(), HttpStatus.OK);
 
         ResponseEntity<GameResult> response = gameEngineController.createGame(postGameInput);
 
-        assertEquals(expected, response);
+        assertEquals(expectedResponse, response);
         assertEquals(HttpStatus.OK, response.getStatusCode());
     }
 }
