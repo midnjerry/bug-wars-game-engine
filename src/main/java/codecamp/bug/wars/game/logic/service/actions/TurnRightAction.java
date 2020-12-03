3d402ef75e20bb3da2be05d02fb05e90260e6982
Valeria Meza <valeriameza173@gmail.com>
2020-12-02 21:32:52 -0600

    passed the turn left and right actions.

---
+++ src/main/java/codecamp/bug/wars/game/logic/service/actions/TurnRightAction.java
@@ -0,0 +1,27 @@
+package codecamp.bug.wars.game.logic.service.actions;
+
+import codecamp.bug.wars.game.logic.models.Direction;
+import codecamp.bug.wars.game.logic.models.Map;
+import codecamp.bug.wars.game.logic.service.BugRunner;
+
+public class TurnRightAction implements Action{
+    @Override
+    public void execute(BugRunner bugRunner, Map map) {
+
+        switch(bugRunner.getDirection()) {
+
+            case NORTH:
+                bugRunner.setDirection(Direction.EAST);
+                break;
+            case EAST:
+                bugRunner.setDirection(Direction.SOUTH);
+                break;
+            case SOUTH:
+                bugRunner.setDirection(Direction.WEST);
+                break;
+            case WEST:
+                bugRunner.setDirection(Direction.NORTH);
+                break;
+        }
+    }
+}
