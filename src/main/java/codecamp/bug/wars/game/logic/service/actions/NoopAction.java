74017bc31b75d5b9612af04b40ec09696a6466f0
Valeria Meza <valeriameza173@gmail.com>
2020-12-07 21:47:27 -0600

    complete the noop test.

--- src/main/java/codecamp/bug/wars/game/logic/service/actions/NoopAction.java
+++ src/main/java/codecamp/bug/wars/game/logic/service/actions/NoopAction.java
@@ -4,16 +4,10 @@ import codecamp.bug.wars.game.logic.models.GameState;
 import codecamp.bug.wars.game.logic.models.Map;
 import codecamp.bug.wars.game.logic.service.BugRunner;
 
-public class Noop implements Action {
+public class NoopAction implements Action {
 
     @Override
     public void execute(BugRunner bugRunner, Map map) {
-        GameState gameState = new GameState();
-
-        int targetX = bugRunner.getX();
-        int targetY = bugRunner.getY();
-        int tick = gameState.getTick();
-        tick++;
 
     }
 }
