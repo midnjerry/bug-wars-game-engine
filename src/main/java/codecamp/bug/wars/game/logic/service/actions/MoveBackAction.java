727f386cf5669dd660a332f3842d7dab24156d37
Ramon Hubbell <Ramon.hubbell@gmail.com>
2020-12-14 20:38:19 -0600

    feature: finished testing moveBackAction without walls, bugs, or food

---
+++ src/main/java/codecamp/bug/wars/game/logic/service/actions/MoveBackAction.java
@@ -0,0 +1,37 @@
+package codecamp.bug.wars.game.logic.service.actions;
+
+import codecamp.bug.wars.game.logic.models.Direction;
+import codecamp.bug.wars.game.logic.models.Map;
+import codecamp.bug.wars.game.logic.service.engine.BugExecutor;
+import lombok.Data;
+@Data
+public class MoveBackAction implements Action{
+
+    public void execute(BugExecutor bugExecutor, Map map){
+        int targetX =  bugExecutor.getStartingX();
+        int targetY = bugExecutor.getStartingY();
+        switch (bugExecutor.getDirection()){
+            case NORTH:
+                targetY = bugExecutor.getStartingY() + 1;
+                break;
+            case WEST:
+                targetX = bugExecutor.getStartingX() + 1;
+                break;
+            case SOUTH:
+                targetY = bugExecutor.getStartingY() - 1;
+                break;
+            case EAST:
+                targetX = bugExecutor.getStartingX() - 1;
+                break;
+        }
+        bugExecutor.setEndingX(targetX);
+        bugExecutor.setEndingY(targetY);
+
+
+
+
+    }
+
+
+
+}
---
+++ src/test/java/codecamp/bug/wars/game/logic/service/actions/MoveBackActionTest.java
@@ -0,0 +1,120 @@
+package codecamp.bug.wars.game.logic.service.actions;
+
+import codecamp.bug.wars.game.logic.models.Direction;
+import codecamp.bug.wars.game.logic.models.Map;
+import codecamp.bug.wars.game.logic.models.MapSpace;
+import codecamp.bug.wars.game.logic.models.MapSpaceRow;
+import codecamp.bug.wars.game.logic.service.engine.BugExecutor;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.util.Arrays;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class MoveBackActionTest {
+    MoveBackAction moveBackAction;
+    Map map;
+    BugExecutor bug;
+
+    @BeforeEach
+    public void runBeforeEveryTest(){
+        List<MapSpaceRow> rows = Arrays.asList(
+                new MapSpaceRow(Arrays.asList(MapSpace.OPEN, MapSpace.OPEN, MapSpace.OPEN, MapSpace.OPEN, MapSpace.OPEN)),
+                new MapSpaceRow(Arrays.asList(MapSpace.OPEN, MapSpace.OPEN, MapSpace.OPEN, MapSpace.OPEN, MapSpace.OPEN)),
+                new MapSpaceRow(Arrays.asList(MapSpace.OPEN, MapSpace.OPEN, MapSpace.OPEN, MapSpace.OPEN, MapSpace.OPEN)),
+                new MapSpaceRow(Arrays.asList(MapSpace.OPEN, MapSpace.OPEN, MapSpace.OPEN, MapSpace.WALL, MapSpace.OPEN)),
+                new MapSpaceRow(Arrays.asList(MapSpace.OPEN, MapSpace.OPEN, MapSpace.OPEN, MapSpace.OPEN, MapSpace.OPEN))
+        );
+        map = new Map(null, rows, null, null);
+        bug = new BugExecutor();
+        moveBackAction = new MoveBackAction();
+    }
+
+    @Test
+    public void movesBackSouth_incrementsY(){
+        // arrange
+        bug.setStartingX(1);
+        bug.setStartingY(2);
+        bug.setDirection(Direction.NORTH);
+
+        // act
+        moveBackAction.execute(bug, map);
+
+        // assert
+        assertEquals(1,bug.getEndingX());
+        assertEquals(3,bug.getEndingY());
+
+        // arrange
+        bug.setStartingX(bug.getEndingX());
+        bug.setStartingY(bug.getEndingY());
+
+        // act
+        moveBackAction.execute(bug, map);
+
+        // assert
+        assertEquals(1,bug.getEndingX());
+        assertEquals(4,bug.getEndingY());
+        assertEquals(Direction.NORTH, bug.getDirection());
+    }
+
+    @Test
+    public void moveBackEast_incrementX(){
+        // arrange
+        bug.setStartingX(2);
+        bug.setStartingY(1);
+        bug.setDirection(Direction.WEST);
+
+        // act
+        moveBackAction.execute(bug, map);
+
+        // assert
+        assertEquals(3,bug.getEndingX());
+        assertEquals(1,bug.getEndingY());
+        assertEquals(Direction.WEST, bug.getDirection());
+    }
+
+    @Test
+    public void movesBackNorth_decrementsY(){
+        // arrange
+        bug.setStartingX(3);
+        bug.setStartingY(3);
+        bug.setDirection(Direction.SOUTH);
+
+        // act
+        moveBackAction.execute(bug, map);
+
+        // assert
+        assertEquals(3,bug.getEndingX());
+        assertEquals(2,bug.getEndingY());
+
+        // arrange
+        bug.setStartingX(bug.getEndingX());
+        bug.setStartingY(bug.getEndingY());
+
+        // act
+        moveBackAction.execute(bug, map);
+
+        // assert
+        assertEquals(3,bug.getEndingX());
+        assertEquals(1,bug.getEndingY());
+        assertEquals(Direction.SOUTH, bug.getDirection());
+    }
+
+    @Test
+    public void moveBackWest_decrementX(){
+        // arrange
+        bug.setStartingX(2);
+        bug.setStartingY(1);
+        bug.setDirection(Direction.EAST);
+
+        // act
+        moveBackAction.execute(bug, map);
+
+        // assert
+        assertEquals(1,bug.getEndingX());
+        assertEquals(1,bug.getEndingY());
+        assertEquals(Direction.EAST, bug.getDirection());
+    }
+}
